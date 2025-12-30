e854444f9d18fe8201df946a71e59acdda8f78f5
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-12-30 02:51:08 +0200

    chore: muhaha the evil @SuppressWarnings wins

--- common/src/main/java/awildgoose/wylan/WylanMod.java
+++ common/src/main/java/awildgoose/wylan/WylanMod.java
@@ -14,6 +14,7 @@ import net.minecraft.world.item.ItemStack;
 
 import java.util.function.Function;
 
+@SuppressWarnings("unused")
 public final class WylanMod {
     public static final String MOD_ID = "wylan";
 
