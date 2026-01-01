f7441c10f7b1b8dab511d6cfd2d0279ed93d6354
awildergoose <61917452+awildergoose@users.noreply.github.com>
2026-01-01 23:37:15 +0200

    chore: fix translucent blocks on neoforge

---
+++ common/src/main/java/awildgoose/wylan/mixin/client/TranslucentBlocksMixin.java
@@ -0,0 +1,25 @@
+package awildgoose.wylan.mixin.client;
+
+
+import awildgoose.wylan.init.ModBlocks;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
+import net.minecraft.world.level.block.Block;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(ItemBlockRenderTypes.class)
+public class TranslucentBlocksMixin {
+	@Shadow @Final private static Map<Block, ChunkSectionLayer> TYPE_BY_BLOCK;
+
+	@Inject(method = "<clinit>*", at = @At("RETURN"))
+	private static void onInitialize(CallbackInfo ci) {
+		TYPE_BY_BLOCK.put(ModBlocks.KAT_PLUSHIE.get(), ChunkSectionLayer.CUTOUT);
+	}
+}
--- fabric/src/main/java/awildgoose/wylan/fabric/client/WylanModFabricClient.java
+++ fabric/src/main/java/awildgoose/wylan/fabric/client/WylanModFabricClient.java
@@ -1,14 +1,10 @@
 package awildgoose.wylan.fabric.client;
 
-import awildgoose.wylan.init.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
-import net.minecraft.client.renderer.chunk.ChunkSectionLayer;
 
 public final class WylanModFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        // TODO port to stupid neoforge
-        BlockRenderLayerMap.putBlock(ModBlocks.KAT_PLUSHIE.get(), ChunkSectionLayer.CUTOUT);
+
     }
 }
