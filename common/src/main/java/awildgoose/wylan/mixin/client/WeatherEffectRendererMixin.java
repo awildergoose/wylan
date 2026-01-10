00510d9893078f2d92eeef4109ddb9952113fa45
awildergoose <61917452+awildergoose@users.noreply.github.com>
2026-01-10 19:18:03 +0200

    midsave: blood rain 4

--- common/src/main/java/awildgoose/wylan/mixin/client/WeatherEffectRendererMixin.java
+++ common/src/main/java/awildgoose/wylan/mixin/client/WeatherEffectRendererMixin.java
@@ -1,29 +1,24 @@
 package awildgoose.wylan.mixin.client;
 
+import awildgoose.wylan.WylanMod;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.WeatherEffectRenderer;
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(WeatherEffectRenderer.class)
 public class WeatherEffectRendererMixin {
-	@ModifyVariable(
-			method = "renderInstances",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/util/ARGB;white(F)I"
-			)
-	)
-	private int blood(float alpha) {
-		int alphaInt = Math.min(255, Math.max(0, (int)(alpha * 255)));
+	@Unique private static final ResourceLocation BLOOD_RAIN_LOCATION = ResourceLocation.fromNamespaceAndPath(WylanMod.MOD_ID,
+																											  "textures/environment/blood.png");
+	@Shadow @Final @Mutable private static ResourceLocation RAIN_LOCATION;
 
-		int red = 178;
-		int green = 34;
-		int blue = 34;
-
-		return (alphaInt << 24) | (red << 16) | (green << 8) | blue;
+	@Inject(method = "<clinit>", at = @At("TAIL"))
+	private static void modifyRainTexture(CallbackInfo ci) {
+		RAIN_LOCATION = BLOOD_RAIN_LOCATION;
 	}
 }
